6c3b0c9dff3a228ab6457ea3431e79a6d14c2623
Jiwon Cha <jc4va@virginia.edu>
2019-12-09 16:35:15 +0900

    style image url

--- code/GetStyleInfo.js
+++ code/GetStyleInfo.js
@@ -4,8 +4,12 @@ module.exports.function = function getStlyeInfo(temperature, style, point) {
   var weather = require('/utils/GetWeatherAPI.js')
   var attire = require('/utils/GetAttireInfo.js')
 
-  var feelsLike = Math.round(weather.getWinChillTempAPI(point))
+  var latitude = point.latitude
+  var longitude = point.longitude
+
+  var feelsLike = Math.round(weather.getDarkSkyCurrentAPI(latitude, longitude).feelsLike)
   var month = dates.ZonedDateTime.now().getDateTime().date.month
+  var season = ""
 
   if (Object.keys(temperature).length != 0){
     var top = attire.getAttireInfo(temperature).top
@@ -15,6 +19,16 @@ module.exports.function = function getStlyeInfo(temperature, style, point) {
     var bottom = attire.getAttireInfo(feelsLike).bottom
   }
 
+  if (feelsLike < 0) {
+    season = "winter"
+  } else if(1 <= feelsLike && feelsLike <= 10){
+      season = "autumn"
+  } else if(11 <= feelsLike && feelsLike <= 20){
+      season = "spring"
+  } else{
+      season = "summer"
+  }
+
   var attireInfo = {
     top: top, 
     bottom: bottom
@@ -22,11 +36,11 @@ module.exports.function = function getStlyeInfo(temperature, style, point) {
 
   // var styleUrl = "www.google.com/search?q=site%3Anaver.com+가을옷&tbm=isch"
   var styleImage = [
-    "images/autumn/1.jpg",
-    "images/autumn/2.jpg",
-    "images/autumn/3.jpg",
-    "images/autumn/4.jpg",
-    "images/autumn/5.jpg"
+    "images/" + season + "/1.jpg",
+    "images/" + season + "/2.jpg",
+    "images/" + season + "/3.jpg",
+    "images/" + season + "/4.jpg",
+    "images/" + season + "/5.jpg"
   ]
 
   var styleUrl = "https://www.styleshare.kr/feed/dailylook"
